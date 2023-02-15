170c07d25daaf8d08436aad58cda01b57479f8ea
ysancak <yusufsancakk@gmail.com>
2023-02-16 01:04:45 +0300

    adding colors and strings

--- app/utils/strings.ts
+++ app/utils/strings.ts
@@ -1,3 +1,10 @@
 export default {
-    
+    appName: 'PlantApp',
+    welcomeTo: 'Welcome to',
+    getStartedScreenProposion: 'Identify more than 3000+ plants and 88% accuracy.',
+    getStarted: 'Get Started',
+    termsAcceptDescription: 'By tapping next, you are agreeing to PlantID {$1} & {$2}.',
+    privacyPolicy: 'Privacy Policy',
+    termsOfUse: 'Terms of Use',
+    continue: 'Continue',
 }

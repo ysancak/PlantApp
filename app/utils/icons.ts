14ee9bab2f50e3399c320fae4ddd204143aa1099
ysancak <yusufsancakk@gmail.com>
2023-02-16 23:03:10 +0300

    changed icons settings and adding new icons

--- app/utils/icons.ts
+++ app/utils/icons.ts
@@ -1,5 +1,13 @@
+import Leaf from '@app/assets/icons/leaf.svg';
+import Scanner from '@app/assets/icons/scanner.svg';
+import SpeedoMeter from '@app/assets/icons/speedometer.svg';
+import Close from '@app/assets/icons/close.svg';
+
 const Icons = {
- 
+ Leaf,
+ Scanner,
+ SpeedoMeter,
+ Close
 }
 
 export default Icons
--- metro.config.js
+++ metro.config.js
@@ -1,24 +1,16 @@
 const { getDefaultConfig } = require("metro-config");
-const reactNativeSvgTransformer = require.resolve("react-native-svg-transformer");
 
-module.exports = async () => {
+module.exports = (async () => {
   const {
     resolver: { sourceExts, assetExts }
   } = await getDefaultConfig();
-
   return {
     transformer: {
-      getTransformOptions: async () => ({
-        transform: {
-          experimentalImportSupport: false,
-          inlineRequires: true,
-        },
-      }),
-      babelTransformerPath: reactNativeSvgTransformer
+      babelTransformerPath: require.resolve("react-native-svg-transformer")
     },
     resolver: {
       assetExts: assetExts.filter(ext => ext !== "svg"),
       sourceExts: [...sourceExts, "svg"]
     }
   };
-};
+})();

c0572fd0da8c7008992cc359a0a40ddc1cbe836c
ysancak <yusufsancakk@gmail.com>
2023-02-16 22:43:24 +0300

    adding new sizes to title component

--- app/components/title.tsx
+++ app/components/title.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import { View, Text, Image, StyleSheet } from 'react-native';
 
-import { FontFamily, FontSize } from '@app/utils';
+import { FontFamily, FontSize, Colors } from '@app/utils';
 
 export type Props = {
   weight?: FontFamily.light | FontFamily.regular | FontFamily.medium | FontFamily.bold | FontFamily.semiBold | FontFamily.extraBold;
-  size?: FontSize.smallTitle | FontSize.title | FontSize.title2;
+  size?: FontSize.smallTitle | FontSize.title | FontSize.title2 | FontSize.small4;
+  color?: Colors;
+  opacity?: number;
   underline?: boolean;
   children: string;
 };
 
-const Title: React.FC<Props> = ({ weight = FontFamily.regular, size = FontSize.title, underline = false, children }) => {
+const Title: React.FC<Props> = ({ weight = FontFamily.regular, size = FontSize.title, color = Colors.black, opacity = 1, underline = false, children }) => {
   if (underline) {
     return (
       <View>
-        <Text style={{ fontFamily: weight, fontSize: size }}>{children}</Text>
+        <Text style={{ fontFamily: weight, fontSize: size, opacity, color }}>{children}</Text>
         <Image style={styles.underlineImage} source={require('@app/assets/images/general/brush-line.png')} />
       </View>
     );
   }
-  return <Text style={{ fontFamily: weight, fontSize: size }}>{children}</Text>;
+  return <Text style={{ fontFamily: weight, fontSize: size, opacity, color }}>{children}</Text>;
 };
 
 const styles = StyleSheet.create({

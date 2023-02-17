5acbad6f387b2830a5f8534cef57945dbeb60686
ysancak <yusufsancakk@gmail.com>
2023-02-17 22:32:55 +0300

    adding premium box component to feed

---
+++ app/components/feed-premium.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import MaskedView from '@react-native-masked-view/masked-view';
+import LinearGradient from 'react-native-linear-gradient';
+
+import { FontFamily, FontSize, Colors, Icons } from '@app/utils';
+import Typography from './typography';
+
+export type Props = {};
+
+const FeedPremium: React.FC<Props> = ({}) => {
+  return (
+    <TouchableOpacity activeOpacity={0.8} style={styles.container}>
+      <View style={styles.contentContainer}>
+        <Icons.PremiumMessage width={56} height={56} style={{ marginTop: 2 }} />
+        <View style={{ flex: 1 }}>
+          <MaskedView
+            style={{ height: 24 }}
+            maskElement={
+              <Typography size={FontSize.body} weight={FontFamily.medium}>
+                FREE Premium Available
+              </Typography>
+            }>
+            <LinearGradient colors={['#E5C990', '#E4B046']} start={{ x: 0, y: 6 }} end={{ x: 1, y: 2 }} style={{ flex: 1 }} />
+          </MaskedView>
+          <MaskedView style={{ height: 24 }} maskElement={<Typography size={FontSize.small4}>Tap to upgrade your account!</Typography>}>
+            <LinearGradient colors={['#F5C25B', '#FFDE9C']} start={{ x: 0, y: 6 }} end={{ x: 1, y: 2 }} style={{ flex: 1 }} />
+          </MaskedView>
+        </View>
+      </View>
+      <View style={{ marginTop: -6 }}>
+        <Icons.PremiumArrow width={28} height={28} />
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.brown,
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 12,
+    padding: 8,
+    paddingBottom: 2,
+    justifyContent: 'space-between'
+  },
+  contentContainer: {
+    flexDirection: 'row',
+    gap: 8,
+    alignItems: 'center',
+    flex: 1
+  }
+});
+
+export default FeedPremium;

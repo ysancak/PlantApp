319c57c4a9a50975d72951dedc2212d621887d66
ysancak <yusufsancakk@gmail.com>
2023-02-17 22:32:17 +0300

    adding new icons

--- app/utils/icons.ts
+++ app/utils/icons.ts
@@ -2,12 +2,18 @@ import Leaf from '@app/assets/icons/leaf.svg';
 import Scanner from '@app/assets/icons/scanner.svg';
 import SpeedoMeter from '@app/assets/icons/speedometer.svg';
 import Close from '@app/assets/icons/close.svg';
+import Search from '@app/assets/icons/search.svg';
+import PremiumMessage from '@app/assets/icons/premium-message.svg';
+import PremiumArrow from '@app/assets/icons/premium-arrow.svg';
 
 const Icons = {
  Leaf,
  Scanner,
  SpeedoMeter,
- Close
+ Close,
+ Search,
+ PremiumMessage,
+ PremiumArrow
 }
 
 export default Icons

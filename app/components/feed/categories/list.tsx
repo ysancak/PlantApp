a95010c43cfca4ce4f7936df7df50db12fdf2168
ysancak <yusufsancakk@gmail.com>
2023-02-19 18:44:07 +0300

    connected with category and question redux

--- app/components/feed/categories/list.tsx
+++ app/components/feed/categories/list.tsx
@@ -1,101 +1,34 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
 import { SimpleGrid } from 'react-native-super-grid';
 
 import { Padding } from '@app/utils';
 import FeedCategoriesItem from './item';
+import FeedCategoriesLoading from '@app/components/loading/feed/categories';
 
-const categories = [
-  {
-    id: 11,
-    name: 'fern',
-    createdAt: '2023-01-11T10:53:05.801Z',
-    updatedAt: '2023-01-11T10:54:30.059Z',
-    publishedAt: '2023-01-11T10:53:07.416Z',
-    title: 'Ferns',
-    rank: 0,
-    image: {
-      id: 23,
-      name: '6.png',
-      alternativeText: null,
-      caption: null,
-      width: 116,
-      height: 126,
-      formats: null,
-      hash: '6_edbcc6988a',
-      ext: '.png',
-      mime: 'image/png',
-      size: 8.24,
-      url: 'https://cms-cdn.plantapp.app/6_edbcc6988a/6_edbcc6988a.png',
-      previewUrl: null,
-      provider: '@strapi-community/strapi-provider-upload-google-cloud-storage',
-      provider_metadata: null,
-      createdAt: '2023-01-11T10:44:46.151Z',
-      updatedAt: '2023-01-11T10:44:46.151Z'
-    }
-  },
-  {
-    id: 10,
-    name: 'cacti-and-succulent',
-    createdAt: '2023-01-11T10:52:28.521Z',
-    updatedAt: '2023-01-11T10:54:39.391Z',
-    publishedAt: '2023-01-11T10:52:36.428Z',
-    title: 'Cacti and Succulents',
-    rank: 1,
-    image: {
-      id: 25,
-      name: '5.png',
-      alternativeText: null,
-      caption: null,
-      width: 158,
-      height: 152,
-      formats: null,
-      hash: '5_d2384a3938',
-      ext: '.png',
-      mime: 'image/png',
-      size: 10.01,
-      url: 'https://cms-cdn.plantapp.app/5_d2384a3938/5_d2384a3938.png',
-      previewUrl: null,
-      provider: '@strapi-community/strapi-provider-upload-google-cloud-storage',
-      provider_metadata: null,
-      createdAt: '2023-01-11T10:50:17.828Z',
-      updatedAt: '2023-01-11T10:51:05.935Z'
-    }
-  },
-  {
-    id: 4,
-    name: 'flowering',
-    createdAt: '2023-01-11T10:44:18.862Z',
-    updatedAt: '2023-01-11T10:54:54.326Z',
-    publishedAt: '2023-01-11T10:44:20.185Z',
-    title: 'Flowering Plants',
-    rank: 2,
-    image: {
-      id: 22,
-      name: '2.png',
-      alternativeText: null,
-      caption: null,
-      width: 116,
-      height: 126,
-      formats: null,
-      hash: '2_4a226c9ae7',
-      ext: '.png',
-      mime: 'image/png',
-      size: 5.28,
-      url: 'https://cms-cdn.plantapp.app/2_4a226c9ae7/2_4a226c9ae7.png',
-      previewUrl: null,
-      provider: '@strapi-community/strapi-provider-upload-google-cloud-storage',
-      provider_metadata: null,
-      createdAt: '2023-01-11T10:44:13.779Z',
-      updatedAt: '2023-01-11T10:44:13.779Z'
-    }
-  }
-];
+import { getCategories } from '@app/services/api';
+import { useSelector } from 'react-redux';
+import { ICategory } from '@app/store/slices/categories';
 
 export type Props = {};
 
 const FeedCategories: React.FC<Props> = ({}) => {
-  return <SimpleGrid listKey={'id'} spacing={10} style={styles.container} data={categories} maxItemsPerRow={2} renderItem={({ item }) => <FeedCategoriesItem name={item.title} image={item.image.url} />} />;
+  const state = useSelector(state => state);
+  const [categories, setCategories] = useState<ICategory[] | []>([]);
+
+  useEffect(() => {
+    getCategories();
+  }, []);
+
+  useEffect(() => {
+    setCategories(state.categories.categories);
+  }, [state.categories]);
+
+  if (categories.length > 0) {
+    return <SimpleGrid listKey={'id'} spacing={10} style={styles.container} data={categories} maxItemsPerRow={2} renderItem={({ item }) => <FeedCategoriesItem name={item.title} image={item.image.url} />} />;
+  } else {
+    return <FeedCategoriesLoading />;
+  }
 };
 
 const styles = StyleSheet.create({
--- app/components/feed/questions/list.tsx
+++ app/components/feed/questions/list.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, StyleSheet, ScrollView } from 'react-native';
 
 import { FontFamily, FontSize, Padding } from '@app/utils';
@@ -6,36 +6,24 @@ import Typography from '@app/components/typography';
 import FeedQuestionsItem from './item';
 import FeedQuestionsLoading from '@app/components/loading/feed/questions';
 
-const questions = [
-  {
-    id: 1,
-    title: 'How to identify plants?',
-    subtitle: 'Life Style',
-    image_uri: 'https://firebasestorage.googleapis.com/v0/b/flora---plant-identifier.appspot.com/o/public%2FCard.png?alt=media',
-    uri: 'https://plantapp.app/blog/identifying-plant-in-10-steps/',
-    order: 1
-  },
-  {
-    id: 2,
-    title: 'Differences Between Species and Varieties?',
-    subtitle: 'Plant Identify',
-    image_uri: 'https://firebasestorage.googleapis.com/v0/b/flora---plant-identifier.appspot.com/o/public%2Fcard2.png?alt=media',
-    uri: 'https://plantapp.app/blog/differences-between-species-and-varieties/',
-    order: 2
-  },
-  {
-    id: 3,
-    title: 'The reasons why the same plant can look different?',
-    subtitle: 'Life Style',
-    image_uri: 'https://firebasestorage.googleapis.com/v0/b/flora---plant-identifier.appspot.com/o/public%2FCard3.png?alt=media',
-    uri: 'https://plantapp.app/blog/same-seeds-but-different-looking-plants/',
-    order: 3
-  }
-];
+import { getQuestions } from '@app/services/api';
+import { useSelector } from 'react-redux';
+import { IQuestion } from '@app/store/slices/questions';
 
 export type Props = {};
 
 const FeedQuestions: React.FC<Props> = ({}) => {
+  const state = useSelector(state => state);
+  const [questions, setQuestions] = useState<IQuestion[] | []>([]);
+
+  useEffect(() => {
+    getQuestions();
+  }, []);
+
+  useEffect(() => {
+    setQuestions(state.questions.questions);
+  }, [state.questions]);
+
   return (
     <View style={styles.container}>
       <View style={styles.titleContainer}>
@@ -43,11 +31,15 @@ const FeedQuestions: React.FC<Props> = ({}) => {
           Get Started
         </Typography>
       </View>
+
       <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={styles.scrollViewContentContainer}>
-        {/*{questions.map((item, index) => {
-          return <FeedQuestionsItem key={`feed-questions-${index}`} image={item.image_uri} name={item.title} />;
-        })}*/}
-        <FeedQuestionsLoading />
+        {questions.length > 0 ? (
+          questions.map((item, index) => {
+            return <FeedQuestionsItem key={`feed-questions-${index}`} image={item.image_uri} name={item.title} />;
+          })
+        ) : (
+          <FeedQuestionsLoading />
+        )}
       </ScrollView>
     </View>
   );

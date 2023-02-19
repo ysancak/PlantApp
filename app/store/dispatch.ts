1202dd50e132707b2b8e5ed18106f4bcb3e2071f
ysancak <yusufsancakk@gmail.com>
2023-02-19 19:39:09 +0300

    adding onBoardingComplete dispatch

--- app/store/dispatch.ts
+++ app/store/dispatch.ts
@@ -1,6 +1,11 @@
 import store from './index';
 import { setCategories } from './slices/categories';
+import { completeBoarding } from './slices/core';
 import { setQuestions } from './slices/questions';
 
+// Core
+export const dispatchCompleteBoarding = () => store.dispatch(completeBoarding());
+// Categories
 export const dispatchCategories = (payload: any) => store.dispatch(setCategories(payload));
+// Questions
 export const dispatchQuestions = (payload: any) => store.dispatch(setQuestions(payload));
